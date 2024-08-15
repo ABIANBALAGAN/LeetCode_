470824db35308b9e6237b83a811e4bd62b577a0e
ABIANBALAGAN <162068441+ABIANBALAGAN@users.noreply.github.com>
2024-08-15 19:53:54 +0530

    Time: 123 ms (58.67%) | Memory: 52 MB (32.00%) - LeetSync

--- 3431-find-the-minimum-cost-array-permutation/find-the-minimum-cost-array-permutation.java
+++ 3431-find-the-minimum-cost-array-permutation/find-the-minimum-cost-array-permutation.java
@@ -1,52 +1,44 @@
 class Solution {
-    //memo[i][j] = min score using rest of indicies if bit mask i is already used, and last num used was j
-    int[][] memo;
-    //val[i][j] = next num used that gave min of memo[i][j], only used to build answer at the end
-    int[][] val;
+    int[][] arr1;
+    int[][] arr2;
     int n;
     public int[] findPermutation(int[] nums) {
         n = nums.length;
-        memo = new int[1<<n][n];
-        val = new int[1<<n][n];
+        arr1 = new int[1<<n][n];
+        arr2 = new int[1<<n][n];
         for(int i = 0; i < (1<<n); i++) {
             for(int j = 0; j < n; j++) {
-                memo[i][j] = Integer.MAX_VALUE;
+                arr1[i][j] = Integer.MAX_VALUE;
             }
         }
-        //rotating perm doesn't change the score
-        //so we always start perm with 0 to get lexicographically smallest perm
-        //bitmask is 1 and prev is 0
         dfs(1, 0, nums);
-
-        //start building answer
         int[] ans = new int[n];
-        int mask = 1;
-        int prev = 0;
+        int m = 1;
+        int p = 0;
         for(int i = 1; i < n; i++) {
-            prev = val[mask][prev];
-            ans[i] = prev;
-            mask += (1<<prev);
+            p = arr2[m][p];
+            ans[i] = p;
+            m += (1<<p);
         }
         return ans;
     }
 
-    public int dfs(int mask, int prev, int[] nums) {
-        if(Integer.bitCount(mask)==n) { //perm is set, return |perm[n - 1] - nums[perm[0]]|
-            return Math.abs(prev-nums[0]);
+    public int dfs(int m, int p, int[] nums) {
+        if(Integer.bitCount(m)==n) { 
+            return Math.abs(p-nums[0]);
         }
-        if(memo[mask][prev]!=Integer.MAX_VALUE) { //if seen current state before, return it
-            return memo[mask][prev];
+        if(arr1[m][p]!=Integer.MAX_VALUE) { 
+            return arr1[m][p];
         }
         for(int i = 0; i < n; i++) {
-            if((mask&(1<<i)) == 0) { //index i is unused, so we try to use i as next element
-                //Math.abs(prev-nums[i]) is added to score, and recursion for minimum value of next state
-                int res = Math.abs(prev-nums[i]) + dfs(mask+(1<<i), i, nums); 
-                if(res < memo[mask][prev]) { //found new minimum, update memo and val
-                    memo[mask][prev] = res;
-                    val[mask][prev] = i;
+            if((m&(1<<i)) == 0) { 
+                int r = Math.abs(p-nums[i]) + dfs(m+(1<<i), i, nums); 
+                if(r < arr1[m][p]) { 
+                    arr1[m][p] = r;
+                    arr2[m][p] = i;
                 }
             }
         }
-        return memo[mask][prev];
+        return arr1[m][p];
     }
 }

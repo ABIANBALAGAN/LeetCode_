fa4265c4f03e368b79d6d9852addb5bbd4fc7932
ABIANBALAGAN <162068441+ABIANBALAGAN@users.noreply.github.com>
2024-08-15 18:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (82.44%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int n = s.length();
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) count[c - 'a']++;
+
+        int maxCount = 0, maxChar = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxChar = i;
+            }
+        }
+
+        if (maxCount > (n + 1) / 2) return "";
+
+        char[] res = new char[n];
+        int index = 0;
+
+        while (count[maxChar]-- > 0) {
+            res[index] = (char) (maxChar + 'a');
+            index += 2;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (count[i]-- > 0) {
+                if (index >= n) index = 1;
+                res[index] = (char) (i + 'a');
+                index += 2;
+            }
+        }
+
+        return new String(res);
+    }
+}

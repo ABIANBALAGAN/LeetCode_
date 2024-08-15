6419b2122975047775412157d5daae7c36c425ae
ABIANBALAGAN <162068441+ABIANBALAGAN@users.noreply.github.com>
2024-08-15 18:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (99.12%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -2,7 +2,11 @@ class Solution {
     public String reorganizeString(String s) {
         int n = s.length();
         int[] count = new int[26];
-        for (char c : s.toCharArray()) count[c - 'a']++;
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count[c - 'a']++;
+        }
 
         int maxCount = 0, maxChar = 0;
         for (int i = 0; i < 26; i++) {
@@ -12,21 +16,25 @@ class Solution {
             }
         }
 
-        if (maxCount > (n + 1) / 2) return "";
+        if (maxCount > (n + 1) / 2) {
+            return "";
+        }
 
         char[] res = new char[n];
         int index = 0;
 
-        while (count[maxChar]-- > 0) {
+        while (count[maxChar] > 0) {
             res[index] = (char) (maxChar + 'a');
             index += 2;
+            count[maxChar]--;
         }
 
         for (int i = 0; i < 26; i++) {
-            while (count[i]-- > 0) {
+            while (count[i] > 0) {
                 if (index >= n) index = 1;
                 res[index] = (char) (i + 'a');
                 index += 2;
+                count[i]--;
             }
         }
 
